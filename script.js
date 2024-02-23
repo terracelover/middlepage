89656006b489b5229c20c066e2d062964e9ee413
terracelover <160361065+terracelover@users.noreply.github.com>
2024-02-24 00:42:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("detailButton").addEventListener("click", function() {
+  // Replace the URL with the desired destination
+  window.location.href = "https://terracelover.github.io/Formysweetboy/";
+});
